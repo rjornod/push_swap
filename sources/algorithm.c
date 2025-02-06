5428becff2094786044bbda19bf9221acd085d31
Roberto Jornod <rojornod@f0r3s15.codam.nl>
2025-02-06 17:48:26 +0100

    minor fix

--- sources/algorithm.c
+++ sources/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: rojornod <rojornod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:49:20 by rojornod          #+#    #+#             */
-/*   Updated: 2025/02/06 17:30:42 by rojornod         ###   ########.fr       */
+/*   Updated: 2025/02/06 17:46:43 by rojornod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ void	push_swap(t_stack *stack)
 		{
 			push_b(stack);
 			push_b(stack);
-			ft_printf("pushed first two elements to b stack");
+			ft_printf("pushed first two elements to b stack\n");
 			print_stacks(stack);
 		}
 	}
 	if (is_sorted(stack) && (stack->elem_count_a == stack->elem_total))
 		ft_printf("\nstack is sorted\n");
-	ft_printf("elem count: [%d]\nelem total [%d]", stack->elem_count_a, stack->elem_total);
+	ft_printf("elem count: [%d]\nelem total [%d]\n", stack->elem_count_a, stack->elem_total);
 }
 
 bool	is_sorted(t_stack *stack)
@@ -34,12 +34,11 @@ bool	is_sorted(t_stack *stack)
 	int	i;
 
 	i = 0;
-	while (i < stack->elem_count_a)
+	while (i < stack->elem_count_a - 1)
 	{
-		if (stack->stack_a[i] < stack->stack_a[i + 1])
-		if (i == stack->elem_count_a)
-			return (true);
+		if (stack->stack_a[i] > stack->stack_a[i + 1])
+			return (false);
 		i++;
 	}
-	return (false);
+	return (true);
 }
